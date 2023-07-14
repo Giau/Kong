d65316670a10e46c6dac859952a9467afe3d7c57
Giau <runtooo@gmail.com>
2023-07-14 15:35:59 +0800

    Initial Commit

---
+++ Kong/KongApp.swift
@@ -0,0 +1,17 @@
+//
+//  KongApp.swift
+//  Kong
+//
+//  Created by Giau on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct KongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
